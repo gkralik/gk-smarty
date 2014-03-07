c534425c7a96f2f93bab7d87fff4b9679ad09952
Gregor Kralik <g.kralik@gmail.com>
2014-03-07 12:17:33 +0100

    add basic versio of SmartyRenderer::render

--- src/GkSmarty/View/SmartyRenderer.php
+++ src/GkSmarty/View/SmartyRenderer.php
@@ -11,6 +11,7 @@ namespace GkSmarty\View;
 
 use Smarty;
 use Zend\View\Model\ModelInterface;
+use Zend\View\Exception;
 use Zend\View\Renderer\RendererInterface;
 use Zend\View\Renderer\TreeRendererInterface;
 use Zend\View\Resolver\ResolverInterface;
@@ -39,8 +40,11 @@ class SmartyRenderer implements RendererInterface, TreeRendererInterface
      */
     protected $view;
 
-    public function __construct(View $view, Smarty $smarty, ResolverInterface $resolver)
-    {
+    public function __construct(
+        View $view,
+        Smarty $smarty,
+        ResolverInterface $resolver
+    ) {
         $this->view = $view;
         $this->smarty = $smarty;
         $this->resolver = $resolver;
@@ -76,16 +80,49 @@ class SmartyRenderer implements RendererInterface, TreeRendererInterface
      * Processes a view script and returns the output.
      *
      * @param  string|ModelInterface $nameOrModel The script/resource process, or a view model
-     * @param  null|array|\ArrayAccess $values Values to use during rendering
+     * @param  array|\ArrayAccess $values Values to use during rendering
+     * @throws \Zend\View\Exception\DomainException
      * @return string The script output.
      */
-    public function render($nameOrModel, $values = null)
+    public function render($nameOrModel, $values = array())
     {
-        // TODO: Implement render() method.
+        $model = null;
+        if ($nameOrModel instanceof ModelInterface) {
+            $model = $nameOrModel;
+            $nameOrModel = $model->getTemplate();
+
+            if (empty($nameOrModel)) {
+                throw new Exception\DomainException(sprintf(
+                    '%s: received View Model, but template is empty',
+                    __METHOD__
+                ));
+            }
+
+            $values = (array)$model->getVariables();
+        }
+
+        if(!$this->canRender($nameOrModel)) {
+            return null;
+        }
+
+        $values['this'] = $this;
+
+        $this->smarty->assign($values);
+
+        $file = $this->resolver->resolve($nameOrModel);
+
+        $this->smarty->setTemplateDir(dirname($file));
+
+        $content = $this->smarty->fetch($file);
+
+        return $content;
     }
 
     /**
      * Can the template be rendered?
+     *
+     * A template can be rendered if the attached resolver can resolve the given
+     * template name.
      * @param $name
      * @return bool
      */
